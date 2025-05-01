9ef1808100d96d8ed59ce020e53528e68ab3f912
ZainNaveed87 <nzain082@gmail.com>
2025-05-01 21:50:31 +0500

    Login & Registration Done

---
+++ Login/login.js
@@ -0,0 +1,48 @@
+var email = document.getElementById("email");
+var password = document.getElementById("pass");
+var email_label = document.getElementById("email_label");
+var username_label = document.getElementById("user_label");
+var password_label = document.getElementById("pass_label");
+
+function login() {
+    if (  password.value == "" && email.value == "") {
+        password_label.innerHTML = "Please enter a password";
+        password_label.style.color = "red";
+        email_label.innerHTML = "Please enter an email";
+        email_label.style.color = "red";
+    }
+   
+  
+   
+    else if (password.value == "") {
+        password_label.innerHTML = "Please enter a password";
+        password_label.style.color = "red";
+    }
+    else if (email.value == "") {
+        email_label.innerHTML = "Please enter an email";
+        email_label.style.color = "red";
+    }
+    else if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+        email_label.innerHTML = "Please enter a valid email address";
+        email_label.style.color = "red";
+    }
+    else if (password.value.length < 6) {
+        password_label.innerHTML = "Password must be at least 6 characters long";
+        password_label.style.color = "red";
+    }
+
+    else
+    {
+        var existingUsers = JSON.parse(localStorage.getItem("user")) || [];
+        var userFound = existingUsers.some(function(user) {
+            return user.email === email.value && user.password === password.value;
+        });
+
+        if (userFound) {
+            alert("Login successful!");
+            window.location.href = "./index.html";
+        } else {
+            alert("Invalid email or password. Please try again.");
+        }
+    }
+}
---
+++ registration/registration.js
@@ -0,0 +1,75 @@
+var username = document.getElementById("Username");
+var email = document.getElementById("email");
+var password = document.getElementById("pass");
+var email_label = document.getElementById("email_label");
+var username_label = document.getElementById("user_label");
+var password_label = document.getElementById("pass_label");
+
+function registration() {
+    if (username.value == "" && password.value == "" && email.value == "") {
+        username_label.innerHTML = "Please enter a Username";
+        username_label.style.color = "red";
+        password_label.innerHTML = "Please enter a password";
+        password_label.style.color = "red";
+        email_label.innerHTML = "Please enter an email";
+        email_label.style.color = "red";
+    }
+    else if (username.value == "" && email.value == "") {
+        username_label.innerHTML = "Please enter a Username";
+        username_label.style.color = "red";
+        email_label.innerHTML = "Please enter an email";
+        email_label.style.color = "red";
+    }
+    else if (password.value == "" && email.value == "") {
+        password_label.innerHTML = "Please enter a password";
+        password_label.style.color = "red";
+        email_label.innerHTML = "Please enter an email";
+        email_label.style.color = "red";
+    }
+    else if (username.value == "" && password.value == "") {
+        username_label.innerHTML = "Please enter a Username";
+        username_label.style.color = "red";
+        password_label.innerHTML = "Please enter a password";
+        password_label.style.color = "red";
+    }
+    else if (username.value == "") {
+        username_label.innerHTML = "Please enter a Username";
+        username_label.style.color = "red";
+    }
+    else if (password.value == "") {
+        password_label.innerHTML = "Please enter a password";
+        password_label.style.color = "red";
+    }
+    else if (email.value == "") {
+        email_label.innerHTML = "Please enter an email";
+        email_label.style.color = "red";
+    }
+    else if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+        email_label.innerHTML = "Please enter a valid email address";
+        email_label.style.color = "red";
+    }
+    else if (password.value.length < 6) {
+        password_label.innerHTML = "Password must be at least 6 characters long";
+        password_label.style.color = "red";
+    }
+    else if (username.value.length < 3) {
+        username_label.innerHTML = "Username must be at least 3 characters long";
+        username_label.style.color = "red";
+    }
+    else {
+        var existingUsers = JSON.parse(localStorage.getItem("user")) || [];
+
+        var user = {
+            username: username.value,
+            email: email.value,
+            password: password.value
+        };
+
+        existingUsers.push(user);
+
+        localStorage.setItem("user", JSON.stringify(existingUsers));
+
+        alert("Registration successful");
+        window.location.href = "login.html";
+    }
+}
