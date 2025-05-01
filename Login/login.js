2fd6dff5b5382b0f90cd8674def05ca3e68fb6d2
ZainNaveed87 <nzain082@gmail.com>
2025-05-02 04:42:06 +0500

    Login Registration Complete Of Ecommerce Website With Javascript

--- Login/login.js
+++ Login/login.js
@@ -3,6 +3,8 @@ var password = document.getElementById("pass");
 var email_label = document.getElementById("email_label");
 var username_label = document.getElementById("user_label");
 var password_label = document.getElementById("pass_label");
+var admin_email = "admin@gmail.com"
+var admin_password = "admin123";
 
 function login() {
     if (  password.value == "" && email.value == "") {
@@ -30,6 +32,21 @@ function login() {
         password_label.innerHTML = "Password must be at least 6 characters long";
         password_label.style.color = "red";
     }
+
+    else if (email.value == admin_email && password.value == admin_password) {
+var code = "123456789"
+var input_code = prompt("Enter The Code Of Admin Panel")
+if( code == input_code)
+{
+
+        alert("Login successful as admin!");
+        window.location.href = "./admin.html";
+    }
+    else
+    {
+        alert("Itna Assan Nhi He Jani Hack Krna")
+    }
+}
 
     else
     {
---
+++ foregt/foregt.js
@@ -0,0 +1,36 @@
+let forget_email = document.getElementById("email")
+let email_label = document.getElementById("email_label")
+function forgot()
+{
+    if (forget_email.value == "") {
+        email_label.innerHTML = "Please enter an email";
+        email_label.style.color = "red";
+    }
+    else if (forget_email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+        email_label.innerHTML = "Please enter a valid email address";
+        email_label.style.color = "red";
+    }
+    else if (forget_email.value == "admin@gmail.com")
+    {
+        alert("Don't Try to hack the system")
+        window.location.href = "./Login.html";
+    }
+else
+{
+
+var existingUsers = JSON.parse(localStorage.getItem("user")) || [];
+var userFound = existingUsers.find(function(user) {
+    return user.email === forget_email.value ;
+});
+
+}
+
+if (userFound) {
+    alert(`Your Passowrd is:  ${userFound.password}`)
+    window.location.href = "./Login.html";
+}
+else {
+email_label.innerHTML = "Email not found"
+email_label.style.color = "red"
+}
+}
