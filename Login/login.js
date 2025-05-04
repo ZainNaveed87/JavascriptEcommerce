9bddfb2e4f4180bd28763e3eb9558119b04aa9c4
ZainNaveed87 <nzain082@gmail.com>
2025-05-04 22:51:05 +0500

    all Function of login register completed

--- Login/login.js
+++ Login/login.js
@@ -63,3 +63,18 @@ if( code == input_code)
         }
     }
 }
+
+function Change_icon() {
+    var password = document.getElementById("pass");
+    var icon = document.getElementById("eye_icon");
+
+    if (password.type === "password") {
+        password.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        password.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
