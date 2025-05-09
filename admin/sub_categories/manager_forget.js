17bcf38324cbce2a4501ac4cf441294dbb6c68f9
ZainNaveed87 <nzain082@gmail.com>
2025-05-10 04:48:45 +0500

    manager forget complete

---
+++ admin/sub_categories/manager_forget.js
@@ -0,0 +1,42 @@
+let forget_email = document.getElementById("email")
+let email_label = document.getElementById("email_label")
+function forgot()
+{
+    if (forget_email.value == "") {
+        email_label.innerHTML = "Please enter an email";
+        email_label.style.color = "red";
+    }
+    else if (forget_email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+        email_label.innerHTML = "Please enter a valid email address";
+        email_label.style.color = "red";
+    }
+    else if (forget_email.value == "admin@gmail.com")
+    {
+        alert("Don't Try to hack the system")
+        window.location.href = "./manager_forget.html";
+    }
+
+else
+{
+  
+
+
+
+
+var existingUsers = JSON.parse(localStorage.getItem("manager")) || [];
+var userFound = existingUsers.find(function(user) {
+    return user.email === forget_email.value ;
+});
+
+
+
+if (userFound) {
+    alert(`Your Passowrd is:  ${userFound.password}`)
+    window.location.href = "./users.html";
+}
+else {
+email_label.innerHTML = "Email not found"
+email_label.style.color = "red"
+}
+}
+}
