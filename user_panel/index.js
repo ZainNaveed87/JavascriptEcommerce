b85957167782b03f373286f1c41534270997454a
ZainNaveed87 <nzain082@gmail.com>
2025-05-19 16:32:27 +0500

    undo all works i done in index

--- user_panel/index.js
+++ user_panel/index.js
@@ -1,22 +1,22 @@
-let popuular_products = document.getElementById("popular");
+// let popuular_products = document.getElementById("popular");
 
-document.addEventListener("DOMContentLoaded", function () {
-    displayProducts();
+// document.addEventListener("DOMContentLoaded", function () {
+//     displayProducts();
 
-    function displayProducts() {
-        const products = JSON.parse(localStorage.getItem("products")) || [];
-        const container = document.getElementById("productCardsContainer");
-        products.forEach((product, index) => {
-            popuular_products.className = "product-card";
+//     function displayProducts() {
+//         const products = JSON.parse(localStorage.getItem("products")) || [];
+//         const container = document.getElementById("productCardsContainer");
+//         products.forEach((product, index) => {
+//             popuular_products.className = "product-card";
 
 
-popuular_products.innerHTML += `
-<div class="popular-card">
-        <img class="popular-card-image" src="${product.image}" alt="${product.name}">
-        <div class="product-head">${product.name}</div>
-        <div class="product-stars">Category :${product.category}</div>
-        <div class="product-para">Price: ${product.price}</div>
-      </div>`;
+// popuular_products.innerHTML += `
+// <div class="popular-card">
+//         <img class="popular-card-image" src="${product.image}" alt="${product.name}">
+//         <div class="product-head">${product.name}</div>
+//         <div class="product-stars">Category :${product.category}</div>
+//         <div class="product-para">Price: ${product.price}</div>
+//       </div>`;
 
 
 
@@ -24,6 +24,6 @@ popuular_products.innerHTML += `
 
             
 
-        });
-    }
-});
+//         });
+//     }
+// });
